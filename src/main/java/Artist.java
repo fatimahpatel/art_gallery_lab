9978348fba163024d30aa8f98bfb187e45fb9286
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-16 14:59:27 +0000

    new Artist class with name + getters and setters

---
+++ src/main/java/Artist.java
@@ -0,0 +1,20 @@
+public class Artist {
+    private String name;
+
+
+    public Artist(String name){
+        this.name = name;
+    }
+
+
+    //getters and setters
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    
+}
